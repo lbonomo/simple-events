9b82d633404e3aa6e7da0ff28147038a847f9cbc
Lucas Bonomo <bonomo.lucas@gmail.com>
2023-07-25 22:24:23 -0300

    First commit

---
+++ src/simple-events.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Simple Events
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     simple-events
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Simple_Events
+ */
+
+// Your code starts here.
